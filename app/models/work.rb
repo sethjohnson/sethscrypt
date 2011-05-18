d12e865c01f24447ab655e17752043434eb0c8cd
Seth <seth.johnson@desiringgod.org>
2011-05-18 01:24:54 -0500

    Got the creation and display of title-only works working swell.

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,7 @@
 class Work < ActiveRecord::Base
   attr_accessible :title
-
-  validates :title,  :presence   => true,
+  attr_accessor :author
+ validates :title,  :presence   => true,
                     :length     => { :maximum => 50 }
+ 
 end
